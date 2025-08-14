a703b2af9b6a65ac492a7e2955c912164ef04cdd
Ujwala <ujwalap801@gmail.com>
2025-08-14 21:27:33 +0530

    Added Stack_Using_Queue.js

--- Stack_Using_Queue.js
+++ Stack_Using_Queue.js
@@ -68,3 +68,70 @@ MyStack.prototype.empty = function() {
 };
 
 
+
+
+// USING ONE QUEUE
+
+var MyStack = function() {
+    this.q1 =[];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.q1.push(x);
+    
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    let n = this.q1.length;
+    for(let i=0;i<n-1;i++)
+    {
+        let firstElement = this.q1.shift();
+        this.q1.push(firstElement);
+    }
+    
+    return this.q1.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    let n = this.q1.length;
+    for(let i=0;i<n-1;i++)
+    {
+        let firstElement = this.q1.shift();
+        this.q1.push(firstElement);
+    }
+    
+    let ans = this.q1.shift();
+    this.q1.push(ans);
+    return ans;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+
+    if(this.q1.length ===0) return true;
+
+    return false;
+
+    
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
